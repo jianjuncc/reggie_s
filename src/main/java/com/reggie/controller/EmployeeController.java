ee43a5cfaf30015f1c5cc5002af45585696bc5d8
shu <2251345401@qq.com>
2022-08-08 14:21:07 +0800

    优化员工查询

--- src/main/java/com/reggie/controller/EmployeeController.java
+++ src/main/java/com/reggie/controller/EmployeeController.java
@@ -92,10 +92,10 @@ public class EmployeeController {
     }
 
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize, String name) {
+    public R<Page<Employee>> page(int page, int pageSize, String name) {
         log.info("page:{},pageSize:{},name:{}",page,pageSize,name);
         //构造分页构造器
-        Page pageInfo = new Page(page,pageSize);
+        Page<Employee> pageInfo = new Page<>(page,pageSize);
         //构造条件构造器
         LambdaQueryWrapper<Employee > lambdaQueryWrapper = new LambdaQueryWrapper<>();
 
