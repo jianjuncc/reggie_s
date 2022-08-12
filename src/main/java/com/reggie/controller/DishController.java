f03c76a09e6b980ebee085a1675f041f1a60bf47
shu <2251345401@qq.com>
2022-08-12 14:22:58 +0800

    更新状态

--- src/main/java/com/reggie/controller/DishController.java
+++ src/main/java/com/reggie/controller/DishController.java
@@ -8,14 +8,41 @@ import com.reggie.entity.Dish;
 import com.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 
 @RestController
 @RequestMapping("/dish")
 @Slf4j
 public class DishController {
+    @Resource
+    DishService service;
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize, String name){
+        log.info("page:{},pageSize:{}, String:{}",page,pageSize,name);
+        //分页构造器
+        Page pageInfo = new Page(page,pageSize);
 
+        //条件构造器
+        LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.like(StringUtils.isNotEmpty(name),Dish::getName, name);
+        //排序时间
+        lambdaQueryWrapper.orderByAsc(Dish::getUpdateTime);
+        //将数据存放到页面
+        service.page(pageInfo,lambdaQueryWrapper);
+        return R.success(pageInfo);
+    }
+
+    @PostMapping("/status/{status}")
+    public R<String> status(@PathVariable int status, String[] ids){
+        log.info(ids.toString());
+        for (String id :ids) {
+            Dish dish = service.getById(id);
+            dish.setStatus(status);
+            service.updateById(dish);
+        }
+        return R.success("状态改变成功");
+    }
 }
