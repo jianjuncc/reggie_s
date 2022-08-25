38d0565231d96b5f13c2991382329a1ff1f5a88a
shu <2251345401@qq.com>
2022-08-25 10:17:20 +0800

    发送短信框架

--- src/main/java/com/reggie/common/GlobalExceptionHandler.java
+++ src/main/java/com/reggie/common/GlobalExceptionHandler.java
@@ -1,14 +1,13 @@
 package com.reggie.common;
 
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.apache.ibatis.jdbc.SQL;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
 
 /**
  * 全局异常处理
@@ -22,7 +21,7 @@ public class GlobalExceptionHandler {
     public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
         if (ex.getMessage().contains("Duplicate entry")) {
             String[] split = ex.getMessage().split(" ");
-            return R.error(split + "用户名重复");
+            return R.error(Arrays.toString(split) + "用户名重复");
         }
         return R.error("未知错误");
     }
---
+++ src/main/java/com/reggie/service/impl/UserImpl.java
@@ -0,0 +1,9 @@
+package com.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.reggie.entity.User;
+import com.reggie.mapper.UserMapper;
+import com.reggie.service.UserService;
+
+public class UserImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}
